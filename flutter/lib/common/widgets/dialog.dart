8841b84ae42eb7b8f9af0b268942a7f6b3d562e0
lining <lining@example.com>
2026-09-19 22:46:16 +0800

    feat: default direct-access port 21201 (rendezvous 21119) to avoid upstream RustDesk ports

--- flutter/lib/common/widgets/dialog.dart
+++ flutter/lib/common/widgets/dialog.dart
@@ -408,7 +408,7 @@ Future<String> changeDirectAccessPort(
                         maxLines: null,
                         keyboardType: TextInputType.number,
                         decoration: InputDecoration(
-                            hintText: '21118',
+                            hintText: '21201',
                             isCollapsed: true,
                             prefix: Text('$currentIP : '),
                             suffix: IconButton(
